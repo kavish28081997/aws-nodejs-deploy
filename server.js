2319de4f7e40e69f3ccc8ae8892af8ef4603e0c8
kavish28081997 <kavish.img@gmail.com>
2025-07-11 01:49:37 +0530

    add

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 
 
 app.get('/api/get',(req,res) => {
-     res.send({message:"hello back to nodejs"})
+     res.send({message:"hello back to nodejs we are writing a node js code"})
 })
 
 
 
-app.listen(8000,() =>{
+app.listen(8000,(req,res) =>{
     console.log("listening to 8000");
 })
