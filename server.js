a150887fe45d40d5bc5d0f03c3d1cecf87b00c73
kavish28081997 <kavish.img@gmail.com>
2025-08-01 04:03:13 +0530

    upd1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 
 app.get('/api/get',(req,res) => {
-     res.send({message:"hello everyone i am here please listen me i want to say something to you"})
+     res.send({message:"hello everyone i am here please listen me i want to say something to you this is the nodejs cicd pipeline which built in aws ec2 server"})
 })
 
 
